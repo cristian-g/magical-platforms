e6d1f081c54075672b1c5cd3eb488649574d9b05
cristian-g <cristian@cristiangonzalez.com>
2018-06-02 01:37:23 +0200

    Afegit cotxe, capses, textures i físiques

--- js/scene.js
+++ js/scene.js
@@ -1,12 +1,42 @@
+'use strict';
+
 var renderer;
 var scene;
 var camera;
 
+Physijs.scripts.worker = 'js/physijs_worker.js';
+Physijs.scripts.ammo = 'ammo.js';
+
+var initScene, render,
+    ground_material, box_material, building_material,
+    render_stats, physics_stats, ground, light,
+    vehicle_body, vehicle, loader;
+
+var input;
+
 function createRenderer(){
-    renderer = new THREE.WebGLRenderer();
-    renderer.setClearColor(0x000000, 1.0);
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    //renderer.setClearColor(0x000000, 1.0);
+    renderer.setClearColor (0xff0000, 1);
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.shadowMap.enabled = true;
+    renderer.shadowMapSoft = true;
+}
+
+function createRenderStats() {
+    render_stats = new Stats();
+    render_stats.domElement.style.position = 'absolute';
+    render_stats.domElement.style.top = '1px';
+    render_stats.domElement.style.zIndex = 100;
+    document.body.appendChild( render_stats.domElement );
+}
+
+function createPhysicsStats() {
+    physics_stats = new Stats();
+    physics_stats.domElement.style.position = 'absolute';
+    physics_stats.domElement.style.top = '50px';
+    physics_stats.domElement.style.zIndex = 100;
+    document.body.appendChild( physics_stats.domElement );
 }
 
 function createCamera(){
@@ -21,16 +51,29 @@ function createCamera(){
     camera.position.z = 32;
     camera.lookAt(scene.position);
 
-    cameraControl = new THREE.OrbitControls(camera);
+    //cameraControl = new THREE.OrbitControls(camera);
 }
 
 function createLight() {
-
-
-    var directionalLight = new THREE.DirectionalLight(0xffffff, 1.0);
+    /*var directionalLight = new THREE.DirectionalLight(0xffffff, 1.0);
     directionalLight.position.set(100, 10, -50);
     directionalLight.name = 'directional';
-    scene.add(directionalLight);
+    scene.add(directionalLight);*/
+    // Light
+    light = new THREE.DirectionalLight( 0xFFFFFF );
+    light.position.set( 20, 20, -15 );
+    light.target.position.copy( scene.position );
+    /*light.castShadow = true;
+    light.shadowCameraLeft = -150;
+    light.shadowCameraTop = -150;
+    light.shadowCameraRight = 150;
+    light.shadowCameraBottom = 150;
+    light.shadowCameraNear = 20;
+    light.shadowCameraFar = 400;
+    light.shadowBias = -.0001
+    light.shadowMapWidth = light.shadowMapHeight = 2048;
+    light.shadowDarkness = .7;*/
+    scene.add( light );
 
     var ambirentLight = new THREE.AmbientLight(0x111111);
     scene.add(ambirentLight);
@@ -108,6 +151,134 @@ function createLee(){
         });
         scene.add(object);
 
+    });
+}
+
+function createGround(ground_material) {
+    var NoiseGen = new SimplexNoise;
+
+    var ground_geometry = new THREE.PlaneGeometry( 300, 300, 100, 100 );
+    for ( var i = 0; i < ground_geometry.vertices.length; i++ ) {
+        var vertex = ground_geometry.vertices[i];
+        //vertex.y = NoiseGen.noise( vertex.x / 30, vertex.z / 30 ) * 1;
+    }
+    ground_geometry.computeFaceNormals();
+    ground_geometry.computeVertexNormals();
+
+    // If your plane is not square as far as face count then the HeightfieldMesh
+    // takes two more arguments at the end: # of x faces and # of z faces that were passed to THREE.PlaneMaterial
+    ground = new Physijs.HeightfieldMesh(
+        ground_geometry,
+        ground_material,
+        0 // mass
+    );
+    ground.rotation.x = -Math.PI / 2;
+    ground.receiveShadow = true;
+    scene.add( ground );
+}
+
+function createBoxes() {
+    for (var i = 0; i < 50; i++) {
+        var size = Math.random() * 2 + .5;
+        var box = new Physijs.BoxMesh(
+            new THREE.BoxGeometry( size, size, size ),
+            box_material
+        );
+        box.castShadow = box.receiveShadow = true;
+        box.position.set(
+            Math.random() * 25 - 50,
+            5,
+            Math.random() * 25 - 50
+        );
+        scene.add( box )
+    }
+}
+
+function createVehicle() {
+    var json_loader = new THREE.JSONLoader();
+
+    json_loader.load( "models/mustang.js", function( car, car_materials ) {
+        json_loader.load( "models/mustang_wheel.js", function( wheel, wheel_materials ) {
+            var mesh = new Physijs.BoxMesh(
+                car,
+                new THREE.MeshFaceMaterial( car_materials )
+            );
+            mesh.position.y = 2;
+            mesh.castShadow = mesh.receiveShadow = true;
+
+            vehicle = new Physijs.Vehicle(mesh, new Physijs.VehicleTuning(
+                10.88,
+                1.83,
+                0.28,
+                500,
+                10.5,
+                6000
+            ));
+            scene.add( vehicle );
+
+            var wheel_material = new THREE.MeshFaceMaterial( wheel_materials );
+
+            for ( var i = 0; i < 4; i++ ) {
+                vehicle.addWheel(
+                    wheel,
+                    wheel_material,
+                    new THREE.Vector3(
+                        i % 2 === 0 ? -1.6 : 1.6,
+                        -1,
+                        i < 2 ? 3.3 : -3.2
+                    ),
+                    new THREE.Vector3( 0, -1, 0 ),
+                    new THREE.Vector3( -1, 0, 0 ),
+                    0.5,
+                    0.7,
+                    i < 2 ? false : true
+                );
+            }
+
+            input = {
+                power: null,
+                direction: null,
+                steering: 0
+            };
+            document.addEventListener('keydown', function( ev ) {
+                switch ( ev.keyCode ) {
+                    case 37: // left
+                        input.direction = 1;
+                        break;
+
+                    case 38: // forward
+                        input.power = true;
+                        break;
+
+                    case 39: // right
+                        input.direction = -1;
+                        break;
+
+                    case 40: // back
+                        input.power = false;
+                        break;
+                }
+            });
+            document.addEventListener('keyup', function( ev ) {
+                switch ( ev.keyCode ) {
+                    case 37: // left
+                        input.direction = null;
+                        break;
+
+                    case 38: // forward
+                        input.power = null;
+                        break;
+
+                    case 39: // right
+                        input.direction = null;
+                        break;
+
+                    case 40: // back
+                        input.power = null;
+                        break;
+                }
+            });
+        });
     });
 }
 
@@ -191,15 +362,88 @@ function createEarthMaterial() {
 
 }
 
+function createGroundMaterial(loader) {
+    ground_material = Physijs.createMaterial(
+        new THREE.MeshLambertMaterial({ map: loader.load( 'images/road.jpg' ) }),
+        .8, // high friction
+        .4 // low restitution
+    );
+    ground_material.map.wrapS = ground_material.map.wrapT = THREE.RepeatWrapping;
+    ground_material.map.repeat.set( 3, 3 );
+    return ground_material;
+}
+
+function createBoxMaterial(loader) {
+    box_material = Physijs.createMaterial(
+        new THREE.MeshLambertMaterial({ map: loader.load( 'images/plywood.jpg' ) }),
+        .4, // low friction
+        .6 // high restitution
+    );
+    box_material.map.wrapS = box_material.map.wrapT = THREE.RepeatWrapping;
+    box_material.map.repeat.set( .25, .25 );
+    return box_material;
+}
+
+function createBuildingMaterial(loader) {
+    building_material = Physijs.createMaterial(
+        new THREE.MeshLambertMaterial({ map: loader.load( 'images/building.jpg' ) }),
+        .4, // low friction
+        .6 // high restitution
+    );
+    building_material.map.wrapS = building_material.map.wrapT = THREE.RepeatWrapping;
+    building_material.map.repeat.set( .25, .25 );
+    return building_material;
+}
+
+
+
 function init() {
 
-    scene = new THREE.Scene();
+    //scene = new THREE.Scene();
+    scene = new Physijs.Scene;
+    scene.setGravity(new THREE.Vector3( 0, -30, 0 ));
+
+    // Loader
+    loader = new THREE.TextureLoader();
+    ground_material = createGroundMaterial(loader);
+    box_material = createBoxMaterial(loader);
+    building_material = createBuildingMaterial(loader);
+
+    scene.addEventListener(
+        'update',
+        function() {
+
+            if ( input && vehicle ) {
+                if ( input.direction !== null ) {
+                    input.steering += input.direction / 50;
+                    if ( input.steering < -.6 ) input.steering = -.6;
+                    if ( input.steering > .6 ) input.steering = .6;
+                }
+                vehicle.setSteering( input.steering, 0 );
+                vehicle.setSteering( input.steering, 1 );
+
+                if ( input.power === true ) {
+                    vehicle.applyEngineForce( 300 );
+                } else if ( input.power === false ) {
+                    vehicle.setBrake( 20, 2 );
+                    vehicle.setBrake( 20, 3 );
+                } else {
+                    vehicle.applyEngineForce( 0 );
+                }
+            }
+
+            scene.simulate( undefined, 2 );
+            physics_stats.update();
+        }
+    );
 
     createRenderer();
+    createRenderStats();
+    createPhysicsStats();
+
     createCamera();
 
     var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    var material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
 
     geometry.applyMatrix( new THREE.Matrix4().makeTranslation( 0, 0.5, 0 ) );
     geometry.faces.splice(6,2);
@@ -235,15 +479,18 @@ function init() {
     var material = new THREE.MeshLambertMaterial();
     material.map = texture;
 
-    var final = new THREE.Mesh(cityGeometry);
+    var final = new THREE.Mesh(cityGeometry, building_material);
 
     console.log(cityGeometry, new THREE.MeshPhongMaterial());
     //var cube = new THREE.Mesh( geometry );
     scene.add( final );
 
+    createGround(ground_material);
+    createBoxes(box_material);
+    createVehicle();
 
-    /*
     createLight();
+    /*
     //createBox();
     //createPlane();
     //createEarth();
@@ -253,17 +500,30 @@ function init() {
 
     document.body.appendChild( renderer.domElement );
 
-    render();
+    //render();
+    requestAnimationFrame( render );
+    scene.simulate();
 }
 
-function render() {
-    cameraControl.update();
+render = function() {
+    //cameraControl.update();
 
     //scene.getObjectByName('earth').rotation.y += 0.005;
     //scene.getObjectByName('cloud').rotation.y += 0.003;
 
-    renderer.render(scene, camera);
-    requestAnimationFrame(render);
+    //renderer.render(scene, camera);
+    //requestAnimationFrame(render);
+
+    requestAnimationFrame( render );
+    if ( vehicle ) {
+        camera.position.copy( vehicle.mesh.position ).add( new THREE.Vector3( 40, 25, 40 ) );
+        camera.lookAt( vehicle.mesh.position );
+
+        //light.target.position.copy( vehicle.mesh.position );
+        //light.position.addVectors( light.target.position, new THREE.Vector3( 20, 20, -15 ) );
+    }
+    renderer.render( scene, camera );
+    render_stats.update();
 }
 
 init();
